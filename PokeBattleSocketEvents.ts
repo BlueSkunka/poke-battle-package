b510d6b2d627b8a287eca5961f54127ff7ff34ba
BlueSkunka <quentin.somveille3874@gmail.com>
2024-11-08 12:15:47 +0100

    feat: init package

---
+++ PokeBattleSocketEvents.ts
@@ -0,0 +1,8 @@
+export const PokeBattleSocketEvents = {
+    CONNECTION: 'connection',
+    DISCONNECT: 'disconnect',
+    TEST_EVENT: 'test_event',
+
+    // Game Event
+    GAME_CREATE_ROOM: 'game_create_room'
+}
