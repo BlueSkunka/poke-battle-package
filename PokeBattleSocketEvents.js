54a3e3716de5713b7c658b2bd49990698859a7fc
BlueSkunka <quentin.somveille3874@gmail.com>
2024-11-09 16:31:38 +0100

    feat: build js file

---
+++ PokeBattleSocketEvents.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PokeBattleSocketEvents = void 0;
+exports.PokeBattleSocketEvents = {
+    CONNECTION: 'connection',
+    DISCONNECT: 'disconnect',
+    TEST_EVENT: 'test_event',
+    // Game Event
+    GAME_CREATE_ROOM: 'game_create_room'
+};
